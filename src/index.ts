3553a0d94a9ba774104b136f55ec10609a94218e
Balaji_Asish <balajiashish02@gmail.com>
2025-02-03 23:06:55 +0530

    first query

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("hi");
+import {PrismaClient} from "@prisma/client"
+
+const client = new PrismaClient();
+
+async function main(){
+await client.todos.create({
+    data:{
+        username: "Asish",
+        name:"Asish",
+        done:false
+    }
+})
+}
+
+main();
